c4ddd5d1cf04482be1c0c62f6de470585f5668b4
guenza <g.yanik@campus.tu-berlin.de>
2021-09-06 12:12:46 +0200

    auto git update within script

--- cython/run.py
+++ cython/run.py
@@ -1,3 +1,4 @@
+from git import Repo
 
 import os
 import time
@@ -5,42 +6,46 @@ import time
 import multiprocessing
 import amazons
 
-def main(times=1,inputfile= "6x6",A=5,B=3,MCTS=100000,cores=1):
-    # 23 21 21
-    # 25 23 21
+
+PATH_OF_GIT_REPO = '../.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'RESULTS'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.index.add(['cython/newres.txt'])
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print("err while pushing")
+    
+
+     
+
+def main(times=1,inputfile= "10x10",A=3,B=5,MCTS=100000,cores=1):
     print(os.cpu_count(), ": CPU COUNT")
-   # times = int(input("times"))
-    #inputfile = input("nxn")
-    #A = int(input("A mode"))
-    #B = int(input("B mode"))
-    #MCTS = int(input("MCTS sim"))
-    #cores = int(input("cores#"))
-    #times=  2
-    #cores = 25
-    #inputfile = "10x10"
-    #Alist = [3,5,1,3,1,5,4,3,4,5]
-    #Blist = [5,3,3,1,5,1,3,4,5,4]
-    #MCTSl = [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000]
-    Alist = [A]
-    Blist = [B]
-    MCTSl =[MCTS]
+ 
+    times = 1
+    cores = 1
+    inputfile = "4x4"
+    Alist = [3]
+    Blist = [5]
+    MCTSl = [1000]
+    #Alist = [A]
+    #Blist = [B]
+    #MCTSl =[MCTS]
     for x in range(len(Alist)):
         processes =[]
         q = multiprocessing.Queue()
         stamp = time.time()
         for i in range(cores):
-            p = multiprocessing.Process(target=amazons.main,args=(i,q,times,inputfile,Alist[x],Blist[x],MCTSl[x], 10)) 
+            p = multiprocessing.Process(target=amazons.main,args=(i,q,times,inputfile,Alist[x],Blist[x],MCTSl[x], 1)) 
             p.start()
             processes.append(p)
         for p in processes:
             p.join()
-
-        results = [q.get() for j in processes]
-        print(results)
-        
-        f = open("testresults.txt", "a")
-        f.write(str(time.time()-stamp)+"\n"+"white wins: "+str(sum(results))+"\n"+str(times*cores)+ "A: "+str(Alist[x])+"B: "+str(Blist[x])+"MCTS: "+str(MCTSl[x])+"\n"+inputfile+str(x)+"\n")
-        f.close()
+        git_push()
 main()
 
     
